061681b005e74708f66c5eae537eed29f3648a0c
Kyung Sang <whrudtkdsla@naver.com>
2017-07-31 12:46:18 +0900

    variables

---
+++ StringDemo.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String first = "coding";
+		
+		String a,b;
+		a = "Hi\n";
+		b = "Hello";
+		
+		System.out.println(first);
+		System.out.println(a+b);
+
+	}
+
+}
