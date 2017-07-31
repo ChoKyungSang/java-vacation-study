aef432739ea542806df8dc512e62dcadb37e3f73
Kyung Sang <whrudtkdsla@naver.com>
2017-07-31 12:45:29 +0900

    NumberString

---
+++ Characterstring.java
@@ -0,0 +1,10 @@
+package numberstring;
+
+public class Characterstring {
+
+	public static void main(String[] args) {
+		System.out.println("egoing said\n \"Welcome\n programming world\"");
+
+	}
+
+}
---
+++ Number.java
@@ -0,0 +1,11 @@
+package numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1.2+1.3);
+
+	}
+
+}
